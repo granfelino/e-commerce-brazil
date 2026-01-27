1a5a1ee8c4ff6892c49485d881b000ded60ea4e8
granfelino <luciusbest@protonmail.com>
2026-01-27 13:09:34 +0100

    Updated analysis & README

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Brazillian e-commerce dataset analysis
+## Brazillian e-commerce dataset analysis
 
 * Data source: [LINK](https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce?select=product_category_name_translation.csv)
 
 ![Data schema](https://i.imgur.com/HRhd2Y0.png)
 Data schema of the dataset (image taken from the source site).
+
+-----
+### Introduction
 
 * This dataset is big. To clean it and validate it I need a bit of time. For now I will focus on what I think is the core of the dataset:
     1. `orders`
@@ -16,10 +19,34 @@ Data schema of the dataset (image taken from the source site).
 * According to the poster of this dataset the data is real and therefore some columns were anonymized (e.g. by replacing companies' names with Game of Thrones house names).
 
 -----
-* I will proceed as follows:
+* I proceeded as follows:
     1. Load the datasets into SQL.
     2. Clean the datasets into SQL & prepare theirs ready-to-analyze versions.
     3. Join & aggregate.
-    4. Load the aggregated data into Excel and Python for furhter analysis and vizualisation.
+    4. Load the aggregated data into Python for furhter analysis and vizualisation.
 
-* I focused on cleaning customers well
+* I focused on cleaning customers well:
+    1. Checking NULLs
+    2. Fake NULLs ('', 'n/a', 'null')
+    3. Flagging the header row
+    4. Looking for impossible values and unnecessary symbols
+    5. Flagging duplicate rows
+    6. Flagging duplicate customer\_id's - since I do not know how to interpret them
+
+* For the customers table I followed the 'raw -> stage -> clean -> analysis pipeline', where I first load the raw data, remove unnecesary symbols, flag duplicates, NULLS in the staging table, then exclude the flagged rows in the clean table & finally create a proper table with correct types & constraints, called 'analysis'.
+* For the rest of the tables I sometimes skipped the clean stage, since there was not much to be cleaned and make it usable. The rest of the tables were made sure to be joinable, checked for NULLs, fake NULLs, duplicates & have had the header row deleted. All of them had correct column types & constraints applied.
+* I created a 'main' table compiling the most important information for a primary analysis. It contains:
+    1. `order_id`
+	2. `customer_id`
+	3. `order_status`
+	4. `item_id`
+	5. `product_id`
+	6. `category`
+	7. `price`
+	7. `shipping`
+	9. `order_date`
+	10. `state` (region)
+
+-----
+### Analysis results
+![Revenue by category](plots/rev-by-cat.png)
--- sql/03-analysis.sql
+++ sql/03-analysis.sql
@@ -12,6 +12,7 @@ SELECT o.order_id,
 	   p.category,
 	   oi.price,
 	   oi.shipping,
+	   o.order_ts AS order_date,
 	   c.state
 FROM orders o
 JOIN order_items oi USING (order_id)
@@ -130,3 +131,96 @@ FROM customer_avg_item
 CROSS JOIN percentiles
 GROUP BY buckets
 ORDER BY buckets;
+
+-- 6. Monthly revenue trend
+CREATE VIEW mom_rev AS 
+WITH monthly_rev AS (
+	SELECT DATE_TRUNC('month', order_date) AS month,
+	   SUM(price) AS revenue
+	FROM main
+	WHERE order_date >= MAKE_DATE(2017, 1, 1)
+	GROUP BY 1
+),
+monthly_prev AS (
+	SELECT *,
+		   LAG(revenue) OVER(ORDER BY month) AS prev_rev
+	FROM monthly_rev
+)
+SELECT month,
+	   revenue,
+	   CASE
+	   		WHEN prev_rev IS NULL THEN NULL
+			ELSE ROUND(((revenue / prev_rev) - 1) * 100, 2)
+	   END  AS monthly_change
+FROM monthly_prev;
+
+-- 7. Yearly revenue trend
+CREATE VIEW yoy_rev AS 
+WITH yearly_rev AS (
+	SELECT DATE_TRUNC('year', order_date) AS year,
+	   SUM(price) AS revenue
+	FROM main
+	WHERE order_date >= MAKE_DATE(2017, 1, 1)
+	GROUP BY 1
+),
+yearly_prev AS (
+	SELECT *,
+		   LAG(revenue) OVER(ORDER BY year) AS prev_rev
+	FROM yearly_rev
+)
+SELECT year,
+	   revenue,
+	   CASE
+	   		WHEN prev_rev IS NULL THEN NULL
+			ELSE ROUND(((revenue / prev_rev) - 1) * 100, 2)
+	   END  AS yearly_change
+FROM yearly_prev;
+
+
+-- 8. Top 3 categories per quarter
+CREATE VIEW quarterly_cat_rev_view AS
+WITH quarterly_cat_rev AS (
+	SELECT DATE_TRUNC('quarter', order_date) AS quarter,
+		   category,
+		   SUM(price) AS total,
+	   		ROW_NUMBER() OVER(
+	   			PARTITION BY DATE_TRUNC('quarter', order_date)
+	   			ORDER BY SUM(price) DESC
+	   		) AS rnk
+	FROM main
+	WHERE order_date >= MAKE_DATE(2017, 1, 1)
+	GROUP BY 1, 2
+)
+SELECT quarter,
+	   category,
+	   total,
+	   rnk
+FROM quarterly_cat_rev
+WHERE rnk <= 3
+ORDER BY 1, 3 DESC;
+
+
+-- 9. Revenue vs volume over time
+CREATE VIEW rev_quant_over_time_view AS
+WITH weekly_rev_quant AS (
+	SELECT DATE_TRUNC('week', order_date) AS week,
+		   SUM(price) AS total_rev,
+		   COUNT(*) AS total_quant
+	FROM main
+	WHERE order_date >= MAKE_DATE(2017, 1, 1)
+	  AND order_date < MAKE_DATE(2018, 8, 27)
+	GROUP BY 1
+),
+first_vals AS (
+	SELECT week,
+		   total_rev,
+		   total_quant,
+		   FIRST_VALUE(total_rev) OVER(ORDER BY total_rev) AS first_rev,
+		   FIRST_VALUE(total_quant) OVER(ORDER BY total_quant) AS first_quant
+	FROM weekly_rev_quant
+)
+SELECT week,
+	   ROUND(total_rev / first_rev * 100, 2) AS revenue_index,
+	   ROUND(total_quant::NUMERIC / first_quant * 100, 2) AS quant_index
+FROM first_vals
+ORDER BY week;
